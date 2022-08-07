6c1d0a0e4d8a5113488fdb67c10edbe08b07e3b0
aquamarin102 <97702538+aquamarin102@users.noreply.github.com>
2022-08-08 02:13:41 +0300

    Homework1

--- Assets/Skripts/Homework1/Task3.cs
+++ Assets/Skripts/Homework1/Task3.cs
@@ -9,28 +9,43 @@ namespace Homework.Homework1
         {
             String _stringNumber;
 
+            string startProgram = "Здравствуйте вас приветствует математическая программа";
+            string entryNumber = "Пожалуйста введите число.";
+            
+            do
+            {
+                Console.WriteLine(startProgram);
+                Console.WriteLine(entryNumber);
+
+                _stringNumber = Console.ReadLine();
+                
+                switch (_stringNumber)
+                {
+                    default:
+                        Fact(_stringNumber);
+                        break;
+                    
+                    case "q":
+                        return;
+                }
+                
+            } while (true);
+
+            
+        }
+
+        static void Fact(String _stringNumber)
+        {
             int _intNumber;
             int _numberFactorial = 1;
             int _sumNumbers = 0;
             int _maxEvenNumber = 0;
-
-            string startProgram = "Здравствуйте вас приветствует математическая программа";
-            string entryNumber = "Пожалуйста введите число.";
+            
             string quitProgram = "q";
             string textNumberFactorial = "Факториал равен ";
             string textSumNumbers = "Сумма от 1 до N равна ";
             string textMaxEvenNumber = "Максимальное четное число меньше N равно";
-
-            Console.WriteLine(startProgram);
-            Console.WriteLine(entryNumber);
-
-            _stringNumber = Console.ReadLine();
-
-            if (_stringNumber == quitProgram)
-            {
-                return;
-            }
-
+            
             _intNumber = Int32.Parse(_stringNumber);
 
             for (int i = 1; i <= _intNumber; i++)
@@ -46,7 +61,6 @@ namespace Homework.Homework1
             Console.WriteLine(textNumberFactorial + _numberFactorial);
             Console.WriteLine(textSumNumbers + _sumNumbers);
             Console.WriteLine(textMaxEvenNumber + _maxEvenNumber);
-            Console.ReadLine();
         }
     }
 }
